f5374d6a6802560a0188a12d9b8059a573468f26
w1ll1x <90297356+w1ll1x@users.noreply.github.com>
2023-05-11 22:19:39 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,50 +4,51 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Load the CSV file into a pandas DataFrame
-tennis_data = pd.read_csv('data/2000.csv')
-
-# Drop unnecessary columns
-tennis_data.drop(['Comment'], axis=1, inplace=True)
-
-# Handle missing values
-tennis_data = tennis_data.replace('NR', np.nan)
-tennis_data.fillna(0, inplace=True)
-
-# Encode categorical features
-tennis_data = pd.get_dummies(tennis_data, columns=['Location', 'Tournament', 'Series', 'Court', 'Surface'])
 
 
-print(tennis_data.head().to_string())
+# Path to directory containing CSV files
+df = pd.read_csv('tennis.csv')
 
-# Create new features
-tennis_data['Rank_diff'] = tennis_data['WRank'].astype(int) - tennis_data['LRank'].astype(int)
-tennis_data['Sets_diff'] = tennis_data['Wsets'].astype(int) - tennis_data['Lsets'].astype(int)
+keep_cols = ['ATP', 'Location', 'Tournament', 'Date', 'Series', 'Court', 'Surface', 'Round', 'Best of', 'Winner', 'Loser', 'WRank', 'LRank', 'WPts', 'LPts', 'W1', 'L1', 'W2', 'L2', 'W3', 'L3', 'W4', 'L4', 'W5', 'L5', 'Wsets', 'Lsets', 'MaxW', 'MaxL', 'AvgW', 'AvgL']
 
+# Drop all columns except the ones in keep_cols
+df = df[keep_cols]
 
+df['Location'] = pd.factorize(df['Location'])[0]
+df['Tournament'] = pd.factorize(df['Tournament'])[0]
+df['Series'] = pd.factorize(df['Series'])[0]
+df['Court'] = pd.factorize(df['Court'])[0]
+df['Surface'] = pd.factorize(df['Surface'])[0]
+df['Round'] = pd.factorize(df['Round'])[0]
 
-# Split the data into training and test sets
-X = tennis_data.drop(['Winner', 'Loser'], axis=1)
-y = tennis_data['Winner']
+df['RankDiff'] = df['WRank'] - df['LRank']
+
+print(df.dtypes)
+
+X = df.drop(['Winner'], axis=1)
+y = df['Winner']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Build the TensorFlow model
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
+# Normalize input data
+X_train = (X_train - X_train.mean()) / X_train.std()
+X_test = (X_test - X_train.mean()) / X_train.std()
+
+# Build neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=[X_train.shape[1]]),
+    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-# Compile the model
+# Compile model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Train the model
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, batch_size=32)
+# Train model
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
-# Evaluate the model
-y_pred = model.predict_classes(X_test)
+# Evaluate model on test set
+y_pred = model.predict(X_test)
+y_pred = np.round(y_pred).astype(int)
 accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
+print(f'Test accuracy: {accuracy:.3f}')
 
